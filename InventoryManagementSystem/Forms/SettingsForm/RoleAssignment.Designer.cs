f16c92444430d65d984d2029f7a69fae24263a07
LLOYD ACOSTA <LLOYD ACOSTA@LAPTOP-1I9L5AGN>
2024-05-05 00:41:57 +0800

    Adding Role Access Design

---
+++ InventoryManagementSystem/Forms/SettingsForm/RoleAssignment.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace InventoryManagementSystem.Forms.SettingsForm
+{
+    partial class RoleAssignment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RoleAssignment));
+            panelBg = new Panel();
+            pictureBox1 = new PictureBox();
+            closeBtn = new Button();
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            listBox1 = new ListBox();
+            groupBox2 = new GroupBox();
+            addBtn = new Button();
+            comboBox1 = new ComboBox();
+            panelBg.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelBg
+            // 
+            panelBg.BackColor = Color.DimGray;
+            panelBg.Controls.Add(pictureBox1);
+            panelBg.Controls.Add(closeBtn);
+            panelBg.Controls.Add(label1);
+            panelBg.Dock = DockStyle.Top;
+            panelBg.Location = new Point(0, 0);
+            panelBg.Name = "panelBg";
+            panelBg.Size = new Size(663, 36);
+            panelBg.TabIndex = 29;
+            panelBg.Paint += panelBg_Paint;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(5, 3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(31, 29);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 15;
+            pictureBox1.TabStop = false;
+            // 
+            // closeBtn
+            // 
+            closeBtn.BackgroundImage = Properties.Resources.close__1_;
+            closeBtn.BackgroundImageLayout = ImageLayout.Zoom;
+            closeBtn.Cursor = Cursors.Hand;
+            closeBtn.Dock = DockStyle.Right;
+            closeBtn.FlatAppearance.BorderSize = 0;
+            closeBtn.FlatStyle = FlatStyle.Flat;
+            closeBtn.Location = new Point(631, 0);
+            closeBtn.Name = "closeBtn";
+            closeBtn.Size = new Size(32, 36);
+            closeBtn.TabIndex = 1;
+            closeBtn.UseVisualStyleBackColor = true;
+            closeBtn.Click += closeBtn_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(42, 11);
+            label1.Name = "label1";
+            label1.Size = new Size(121, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Add Role Assignment";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(listBox1);
+            groupBox1.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox1.Location = new Point(8, 50);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(335, 307);
+            groupBox1.TabIndex = 30;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "List of Employees";
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = SystemColors.Control;
+            listBox1.BorderStyle = BorderStyle.None;
+            listBox1.Cursor = Cursors.Hand;
+            listBox1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Items.AddRange(new object[] { "Employee 1", "Employee 2", "Employee 3", "Employee 4", "Employee 5", "Employee 1", "Employee 2", "Employee 3", "Employee 4", "Employee 5", "Employee 1", "Employee 2", "Employee 3", "Employee 4", "Employee 5", "Employee 1", "Employee 2", "Employee 3", "Employee 4", "Employee 5", "Employee 1", "Employee 2", "Employee 3", "Employee 4", "Employee 5", "Employee 1", "Employee 2", "Employee 3", "Employee 4", "Employee 5", "Employee 1", "Employee 2", "Employee 3", "Employee 4", "Employee 5", "Employee 1", "Employee 2" });
+            listBox1.Location = new Point(6, 22);
+            listBox1.Margin = new Padding(30);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(323, 270);
+            listBox1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(addBtn);
+            groupBox2.Controls.Add(comboBox1);
+            groupBox2.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            groupBox2.Location = new Point(370, 50);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(271, 166);
+            groupBox2.TabIndex = 31;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Choose Role";
+            // 
+            // addBtn
+            // 
+            addBtn.BackColor = Color.FromArgb(92, 184, 92);
+            addBtn.Cursor = Cursors.Hand;
+            addBtn.FlatAppearance.BorderSize = 0;
+            addBtn.FlatStyle = FlatStyle.Flat;
+            addBtn.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            addBtn.ForeColor = Color.WhiteSmoke;
+            addBtn.Location = new Point(145, 122);
+            addBtn.Name = "addBtn";
+            addBtn.Size = new Size(116, 30);
+            addBtn.TabIndex = 40;
+            addBtn.Text = "Save";
+            addBtn.UseVisualStyleBackColor = false;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Admin", "User" });
+            comboBox1.Location = new Point(6, 44);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(255, 25);
+            comboBox1.TabIndex = 0;
+            // 
+            // RoleAssignment
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(663, 388);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(panelBg);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "RoleAssignment";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "RoleAssignment";
+            panelBg.ResumeLayout(false);
+            panelBg.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelBg;
+        private PictureBox pictureBox1;
+        private Button closeBtn;
+        private Label label1;
+        private GroupBox groupBox1;
+        private ListBox listBox1;
+        private GroupBox groupBox2;
+        private ComboBox comboBox1;
+        private Button addBtn;
+    }
+}
---
+++ InventoryManagementSystem/Forms/SettingsForm/RoleAssignment.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryManagementSystem.Forms.SettingsForm
+{
+    public partial class RoleAssignment : Form
+    {
+        public Panel PanelBg { get; set; }
+        public static RoleAssignment Instance { get; private set; }
+        public RoleAssignment()
+        {
+            InitializeComponent();
+            PanelBg = panelBg;
+            panelBg.BackColor = Color.DimGray;
+            Instance = this;
+        }
+
+        private void panelBg_Paint(object sender, PaintEventArgs e)
+        {
+            PanelBg.BackColor = Properties.Settings.Default.MyColor;
+        }
+
+        public void ChangePanelColor12(Color color)
+        {
+            PanelBg.BackColor = color;
+        }
+
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
