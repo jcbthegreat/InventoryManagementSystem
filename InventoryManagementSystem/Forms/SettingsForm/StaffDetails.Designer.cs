c4e8748ac26d50add35f1dead8803c9642ea5689
LLOYD ACOSTA <LLOYD ACOSTA@LAPTOP-1I9L5AGN>
2024-05-18 12:25:34 +0800

    adding data on staffdetails

--- InventoryManagementSystem/Forms/SettingsForm/StaffDetails.Designer.cs
+++ InventoryManagementSystem/Forms/SettingsForm/StaffDetails.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StaffDetails));
-            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             panel1 = new Panel();
             button1 = new Button();
             pictureBox1 = new PictureBox();
@@ -39,16 +38,6 @@
             closeBtn = new Button();
             label1 = new Label();
             dataGridView1 = new DataGridView();
-            Column7 = new DataGridViewTextBoxColumn();
-            Column1 = new DataGridViewTextBoxColumn();
-            Column8 = new DataGridViewTextBoxColumn();
-            Column9 = new DataGridViewTextBoxColumn();
-            Column3 = new DataGridViewTextBoxColumn();
-            Column2 = new DataGridViewTextBoxColumn();
-            Column4 = new DataGridViewTextBoxColumn();
-            Column10 = new DataGridViewTextBoxColumn();
-            Column5 = new DataGridViewImageColumn();
-            Column6 = new DataGridViewImageColumn();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             panelBg.SuspendLayout();
@@ -64,7 +53,7 @@
             panel1.Dock = DockStyle.Top;
             panel1.Location = new Point(0, 36);
             panel1.Name = "panel1";
-            panel1.Size = new Size(1055, 100);
+            panel1.Size = new Size(1051, 100);
             panel1.TabIndex = 6;
             panel1.Paint += panel1_Paint;
             // 
@@ -118,7 +107,7 @@
             panelBg.Dock = DockStyle.Top;
             panelBg.Location = new Point(0, 0);
             panelBg.Name = "panelBg";
-            panelBg.Size = new Size(1055, 36);
+            panelBg.Size = new Size(1051, 36);
             panelBg.TabIndex = 5;
             panelBg.Paint += panelBg_Paint;
             // 
@@ -140,7 +129,7 @@
             closeBtn.Dock = DockStyle.Right;
             closeBtn.FlatAppearance.BorderSize = 0;
             closeBtn.FlatStyle = FlatStyle.Flat;
-            closeBtn.Location = new Point(1023, 0);
+            closeBtn.Location = new Point(1019, 0);
             closeBtn.Name = "closeBtn";
             closeBtn.Size = new Size(32, 36);
             closeBtn.TabIndex = 1;
@@ -166,97 +155,20 @@
             dataGridView1.BorderStyle = BorderStyle.None;
             dataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column7, Column1, Column8, Column9, Column3, Column2, Column4, Column10, Column5, Column6 });
             dataGridView1.Cursor = Cursors.Hand;
-            dataGridView1.Location = new Point(18, 142);
+            dataGridView1.Location = new Point(4, 142);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.ReadOnly = true;
             dataGridView1.RowHeadersVisible = false;
             dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(1023, 438);
+            dataGridView1.Size = new Size(1039, 438);
             dataGridView1.TabIndex = 7;
-            // 
-            // Column7
-            // 
-            Column7.HeaderText = "Staff No";
-            Column7.Name = "Column7";
-            Column7.ReadOnly = true;
-            // 
-            // Column1
-            // 
-            Column1.HeaderText = "First name";
-            Column1.Name = "Column1";
-            Column1.ReadOnly = true;
-            // 
-            // Column8
-            // 
-            Column8.HeaderText = "Middle name";
-            Column8.Name = "Column8";
-            Column8.ReadOnly = true;
-            // 
-            // Column9
-            // 
-            Column9.HeaderText = "Last name";
-            Column9.Name = "Column9";
-            Column9.ReadOnly = true;
-            // 
-            // Column3
-            // 
-            Column3.HeaderText = "Email";
-            Column3.Name = "Column3";
-            Column3.ReadOnly = true;
-            Column3.Width = 150;
-            // 
-            // Column2
-            // 
-            Column2.HeaderText = "Contact Number";
-            Column2.Name = "Column2";
-            Column2.ReadOnly = true;
-            Column2.Width = 150;
-            // 
-            // Column4
-            // 
-            Column4.HeaderText = "Address";
-            Column4.Name = "Column4";
-            Column4.ReadOnly = true;
-            Column4.Width = 200;
-            // 
-            // Column10
-            // 
-            Column10.HeaderText = "Username";
-            Column10.Name = "Column10";
-            Column10.ReadOnly = true;
-            // 
-            // Column5
-            // 
-            Column5.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
-            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle2.BackColor = Color.White;
-            dataGridViewCellStyle2.NullValue = resources.GetObject("dataGridViewCellStyle2.NullValue");
-            Column5.DefaultCellStyle = dataGridViewCellStyle2;
-            Column5.Description = "Edit";
-            Column5.HeaderText = "";
-            Column5.Image = Properties.Resources.pencil__1_;
-            Column5.Name = "Column5";
-            Column5.ReadOnly = true;
-            Column5.ToolTipText = "Edit";
-            Column5.Width = 5;
-            // 
-            // Column6
-            // 
-            Column6.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
-            Column6.HeaderText = "";
-            Column6.Image = Properties.Resources.pencil__1_;
-            Column6.Name = "Column6";
-            Column6.ReadOnly = true;
-            Column6.ToolTipText = "Delete";
-            Column6.Width = 5;
             // 
             // StaffDetails
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1055, 592);
+            ClientSize = new Size(1051, 592);
             Controls.Add(dataGridView1);
             Controls.Add(panel1);
             Controls.Add(panelBg);
@@ -287,15 +199,6 @@
         private Button closeBtn;
         private Label label1;
         private DataGridView dataGridView1;
-        private DataGridViewTextBoxColumn Column7;
-        private DataGridViewTextBoxColumn Column1;
-        private DataGridViewTextBoxColumn Column8;
-        private DataGridViewTextBoxColumn Column9;
-        private DataGridViewTextBoxColumn Column3;
-        private DataGridViewTextBoxColumn Column2;
         private DataGridViewTextBoxColumn Column4;
-        private DataGridViewTextBoxColumn Column10;
-        private DataGridViewImageColumn Column5;
-        private DataGridViewImageColumn Column6;
     }
 }
--- InventoryManagementSystem/Forms/SettingsForm/StaffDetails.cs
+++ InventoryManagementSystem/Forms/SettingsForm/StaffDetails.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace InventoryManagementSystem.Forms.SettingsForm
 {
@@ -15,12 +18,22 @@ namespace InventoryManagementSystem.Forms.SettingsForm
         public Panel PanelBg { get; set; }
         public static StaffDetails Instance { get; private set; }
 
+        private readonly SqlConnection connection;
+        SqlDataAdapter adapter;
+        DataTable dt;
+
         public StaffDetails()
         {
             InitializeComponent();
             PanelBg = panelBg;
             panelBg.BackColor = Color.DimGray;
             Instance = this;
+
+            // Initialize the SqlConnection
+            string connectionString = "Server=desktop-eqrn1iv.taile2b728.ts.net;Database=INVENTORY-SYSTEM;User Id=sa;Password=sasa;";
+            connection = new SqlConnection(connectionString);
+
+            showdata();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -38,6 +51,7 @@ namespace InventoryManagementSystem.Forms.SettingsForm
         {
 
         }
+
         public void ChangePanelColor9(Color color)
         {
             PanelBg.BackColor = color;
@@ -51,6 +65,53 @@ namespace InventoryManagementSystem.Forms.SettingsForm
         private void StaffDetails_Load(object sender, EventArgs e)
         {
             PanelBg.BackColor = Properties.Settings.Default.MyColor;
+        }
+
+        public void showdata()
+        {
+            try
+            {
+                // Open the connection
+                connection.Open();
+                adapter = new SqlDataAdapter("SELECT StaffNo as [Staff No],RoleType as [Role],Position,FirstName as [First Name],MiddleName as [Middle Name] "+
+                     ", LastName as [Last Name], Email, ContactNo as [Contact No], UserName as Username FROM IV.StaffAssignment  " +
+                    " order by StaffNo asc", connection);
+                dt = new DataTable();
+                adapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+
+                // Resize the columns based on the content
+                foreach (DataGridViewColumn column in dataGridView1.Columns)
+                {
+                    column.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                }
+
+                dataGridView1.AutoResizeColumns();
+
+                // Remove padding from the cells
+                foreach (DataGridViewColumn column in dataGridView1.Columns)
+                {
+                    column.DefaultCellStyle.Padding = new Padding(0);
+                }
+
+                // Resize the column headers
+                dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+
+                // Ensure all rows are visible
+                dataGridView1.AutoResizeRows(DataGridViewAutoSizeRowsMode.AllCells);
+
+                // Optionally, adjust the last column to fill the remaining space
+                dataGridView1.Columns[dataGridView1.Columns.Count - 1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+            finally
+            {
+                // Ensure the connection is closed
+                connection.Close();
+            }
         }
     }
 }
